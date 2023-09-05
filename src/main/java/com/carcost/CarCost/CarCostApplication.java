fa66c086e378d65f0028e907943fdb98eaeeec8c
Ridvik Pal <pal.ridvik@gmail.com>
2023-09-05 04:51:35 -0400

    Actual first commit

---
+++ src/main/java/com/carcost/CarCost/CarCostApplication.java
@@ -0,0 +1,13 @@
+package com.carcost.CarCost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCostApplication.class, args);
+	}
+
+}
